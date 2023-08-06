ad4b5a322ce09b966f30b4f9a57634a0e4c7fcb0
Vilictus <imvilictus@gmail.com>
2023-08-06 02:03:15 +0000

    v1.1

--- main/app.vue
+++ main/app.vue
@@ -1,6 +1,17 @@
-<script>
+<script setup lang="ts">
 import '@fontsource/barlow';
+
+useHead({
+  title: 'Workspace',
+  meta: [
+    { name: 'description', content: 'No data.' }
+  ],
+  bodyAttrs: {
+    class: 'test'
+  },
+})
 </script>
+
 
 <template>
   <div>
@@ -9,11 +20,10 @@ import '@fontsource/barlow';
 </template>
 
 <style>
-  * {  
-    font-family: 'Barlow';  
-    padding: 0;
-    margin: 0;
-   --fontsource-display: optional;
- }
-  
+* {
+  font-family: 'Barlow';
+  padding: 0;
+  margin: 0;
+  --fontsource-display: optional;
+}
 </style>
