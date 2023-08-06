b7dcaf6245726c6367ebfb0bb048317cbb7c858b
Vilictus <imvilictus@gmail.com>
2023-08-06 01:56:21 +0000

    v1

---
+++ main/app.vue
@@ -0,0 +1,19 @@
+<script>
+import '@fontsource/barlow';
+</script>
+
+<template>
+  <div>
+    <p>Hola mundo</p>
+  </div>
+</template>
+
+<style>
+  * {  
+    font-family: 'Barlow';  
+    padding: 0;
+    margin: 0;
+   --fontsource-display: optional;
+ }
+  
+</style>
